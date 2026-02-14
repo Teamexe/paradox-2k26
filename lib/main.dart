234f122e13e197bed986ebb2a692aa8e04e22d2b
utkarsh684 <shuklautkarsh406@gmail.com>
2026-02-14 20:51:23 +0530

    some thing to commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:paradox/screen/LoginScreen.dart';
-import 'package:paradox/screen/SignupScreen.dart';
-import 'screen/HomeScreen.dart';
-import 'theme/app_theme.dart';
-=======
-import 'package:paradox/minigames/lights_out_screen.dart';
-import 'package:paradox/minigames/mine_scan_screen.dart';
-import 'package:paradox/minigames/multi_task_screen.dart';
-import 'package:paradox/minigames/node_decryption_screen.dart';
-import 'package:paradox/minigames/pathfinder_screen.dart';
 import 'package:paradox/commit_clash_app.dart';
->>>>>>> main
 
 void main() {
   runApp(const MyApp());
@@ -23,25 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-
-      debugShowCheckedModeBanner: false,
-      title: 'Paradox 2K26',
-
-      // customised theme  taken from the app_theme.dart
-      theme: AppTheme.darkGamingTheme,
-      home: const LoginScreen(),
-    );
-  }
-}
-=======
       title: 'Paradox',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0D1117)),
+        useMaterial3: true,
       ),
-      home: PathfinderScreen(),
+      home: const CommitClashApp(), 
     );
   }
 }
->>>>>>> main
